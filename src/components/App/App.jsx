4137c3872ded6d30b238a67de123d0488e79d3e4
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-10-27 10:34:39 +0530

    react router and spring added

---
+++ src/components/App/App.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import {BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom'
+
+export class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          as
+        </Switch>
+      </Router>
+    )
+  }
+}
+
+export default App
