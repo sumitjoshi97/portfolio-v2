cfe42d6258bb422a7b44b84871cb89dfb54d1e82
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-10-29 15:59:56 +0530

    dialog component added

---
+++ src/components/Dialog/index.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './index.scss'
+
+export default class Dialog extends Component {
+  render() {
+    return (
+      <div className="dialog">
+        SUMIT JOSHI
+      </div>
+    )
+  }
+}
