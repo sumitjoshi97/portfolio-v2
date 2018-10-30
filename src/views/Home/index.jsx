296e84035b323d5db8fa60e87a51d66c2fb0fcca
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-10-30 15:29:22 +0530

    NavBar added to Home view

--- src/views/Home/index.jsx
+++ src/views/Home/index.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import './index.scss'
 
+import Nav from '../../components/Nav'
 import Dialog from '../../components/Dialog'
 
 export default class Home extends Component {
   render() {
     return (
       <div className="home">
+        <Nav />
         <Dialog />
       </div>
     )
