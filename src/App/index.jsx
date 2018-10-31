87dd01cb98b3127679ce74c5a2f791df69c8ca9a
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-10-31 14:13:19 +0530

    Adds react-router

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,8 +1,28 @@
-import React, { Component } from 'react'
-import Home from '../views/Home'
+import React, { Component, Suspense } from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import routes from './Routes'
+
+// import './index.scss'
 
 export default class App extends Component {
   render() {
-    return <Home />
+    return (
+      <Router>
+        <Switch>
+          {routes.map(route => (
+            <Route
+              key={route.path}
+              path={route.path}
+              exact={route.exact}
+              render={() => (
+                <Suspense fallback={<div>Loading...</div>}>
+                  <route.component />
+                </Suspense>
+              )}
+            />
+          ))}
+        </Switch>
+      </Router>
+    )
   }
 }
